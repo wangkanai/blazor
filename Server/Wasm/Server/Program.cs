6d7349871edb1e0e1a03ca4faa4a0d2a2fbfce6c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-17 10:12:31 +0700

    migration blazor server wasm to .net8 rtm

--- Server/Wasm/Server/Program.cs
+++ Server/Wasm/Server/Program.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
 using BlazorServerWasm.Client.Pages;
 using BlazorServerWasm.Components;
 
@@ -5,9 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-       .AddInteractiveWebAssemblyComponents();
-
-builder.Services.AddDetection();
+	.AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
@@ -28,10 +28,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.UseDetection();
-
 app.MapRazorComponents<App>()
-   .AddInteractiveWebAssemblyRenderMode()
-   .AddAdditionalAssemblies(typeof(Counter).Assembly);
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(Counter).Assembly);
 
 app.Run();
